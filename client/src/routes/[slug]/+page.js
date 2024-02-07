02fb1c631080abf5fe8cc42be6476e78a203b9f5
msvalen <quequisegarra@gmail.com>
2024-02-07 18:36:37 +0000

    improve error handling

--- client/src/routes/[slug]/+page.js
+++ client/src/routes/[slug]/+page.js
@@ -1,4 +1,9 @@
+import { redirect } from '@sveltejs/kit'
+
 export async function load ({ params }) {
+  if (!['london', 'murcia', 'newport', 'paris', 'ny'].includes(params.slug)) {
+    redirect(302, '/london')
+  }
   let data = await fetch(`http://localhost:8000/location/${params.slug}`)
   let json = await data.json()
   return { data: json, selected: params.slug }
--- client/src/routes/[slug]/+page.svelte
+++ client/src/routes/[slug]/+page.svelte
@@ -2,8 +2,6 @@
 	import { goto } from '$app/navigation';
 	export let data;
   const onChange = (e,value) => {
-    console.log(e.currentTarget.value)
-    console.log(value)
 		goto('/'+e.currentTarget.value)
 	}
 </script>
@@ -12,7 +10,7 @@
 <div class='container p-4 bg-body-secondary bg-opacity-25 rounded-2'>
   <h1 style='-webkit-text-stroke: 0.05em black; color: white;'>Current weather in {data.data.name}:</h1>
   <h2 style='-webkit-text-stroke: 0.05em #198754; color: white;' > {data.data.weather[0].main} <img src='https://openweathermap.org/img/wn/{data.data.weather[0].icon}@2x.png' alt='Weather icon'/> {data.data.main.temp} ºC</h2>
-  <select bind:value={data.selected} class="form-select" aria-label="Default select example" on:change={onChange}>
+  <select bind:value={data.selected} class="form-select" aria-label="Select different location" on:change={onChange}>
     <option value="london">London</option>
     <option value="paris">Paris</option>
     <option value="ny">New York</option>
